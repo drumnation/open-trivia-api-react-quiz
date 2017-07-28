89b47c537f2c954c6b939412a7f1948d7beeb092
drumnation <davidmieloch@gmail.com>
2017-07-28 19:27:38 -0400

    dummy data and question list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,78 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import QuestionList from './quiz/questionList.jsx'
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            questions: [
+                {
+                    id: 1,
+                    text: 'What is your name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Dave'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Tony'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Jim'
+                        }
+                    ],
+                    correct: 'b',
+                },
+                {
+                    id: 2,
+                    text: "What is your mother's name?",
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Deborah'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Allison'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Julie'
+                        }
+                    ],
+                    correct: 'a',
+                }, {
+                    id: 3,
+                    text: "What is your friend's name?",
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Eddie'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Chris'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Matt'
+                        }
+                    ],
+                    correct: 'c',
+                },
+            ],
+            score: 0,
+            current: 1
+        }
+    }
+
     render() {
         return (
             <div>
-                App
+                <QuestionList {...this.state} />
             </div>
         )
     }
---
+++ src/components/quiz/QuestionList.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Question from './Question.jsx'
+
+class QuestionList extends Component {
+    render() {
+        return (
+            <div>
+                QUESTION LIST
+            </div>
+        )
+    }
+}
+
+export default QuestionList
