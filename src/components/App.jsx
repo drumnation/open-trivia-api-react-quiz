6af09690abdce59b1f1e552c7ab3f286733701c4
drumnation <davidmieloch@gmail.com>
2017-07-29 10:06:26 -0400

    Results component shows after all questions are finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import QuestionList from './quiz/questionList.jsx'
-import Scorebox from './quiz/scorebox.jsx'
+import QuestionList from './quiz/QuestionList.jsx'
+import Scorebox from './quiz/Scorebox.jsx'
+import Results from './quiz/Results.jsx'
 
 class App extends Component {
     constructor(props) {
@@ -81,8 +82,10 @@ class App extends Component {
     render() {
         if (this.state.current > this.state.questions.length) {
             var scorebox = ''
+            var results = <Results {...this.state} />
         } else {
             var scorebox = <Scorebox {...this.state} />
+            var results = ''
         }
         return (
             <div>
@@ -92,6 +95,7 @@ class App extends Component {
                     setCurrent={this.setCurrent.bind(this)}
                     setScore={this.setScore.bind(this)}
                 />
+                {results}
             </div>
         )
     }
